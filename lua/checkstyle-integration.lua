3b4e1f5b13e1ca6c5f58f69225313e2929276ff3
TobisMa <tobias-ma@gmx.net>
2025-06-08 20:05:17 +0200

    moved checkstyle analysis into a subprocess so it won't block the main thread

--- lua/checkstyle-integration.lua
+++ lua/checkstyle-integration.lua
@@ -1,5 +1,7 @@
 local M = {};
 
+local uv = vim.uv;
+
 function M.setup(opts)
     if opts.checkstyle_file == nil then
         print("No checkstyle file found")
@@ -10,15 +12,6 @@ function M.setup(opts)
     opts = opts or {};
     if opts.keybind ~= nil then
         vim.keymap.set(opts.keybind.modes or {"n"}, opts.keybind.keys, "<CMD>Jcheck<CR>", opts.keybind.options);
-    end
-
-    if opts.defaultSeverity ~= nil then
-        M.defaultSeverity = opts.defaultSeverity;
-    end
-    if opts.alwaysUseDefaultSeverity == true then
-        M.alwaysUseDefaultSeverity = true;
-    else
-        M.alwaysUseDefaultSeverity = false;
     end
 
     if opts.checkstyle_on_write then
@@ -35,13 +28,81 @@ end
 
 
 function M.java_checkstyle()
-    -- TODO use vim.system() for not blocking
 
-    local output = vim.fn.system({"checkstyle", "-f=plain", "-c", M.checkstyle_file, vim.fn.expand("%")});
     local namespace = vim.api.nvim_create_namespace("checkstyle")
+    local handle;
+    local pid_or_err;
+    local stdin = uv.new_pipe(false)
+    local stdout = uv.new_pipe(false)
+    local stderr = uv.new_pipe(false)
+    local diagnostic_result;
+    local line_count
+    local output = ""
+
+    handle, pid_or_err = uv.spawn("checkstyle", {
+        args = {"-f=plain", "-c", M.checkstyle_file, vim.fn.expand("%")},
+        cwd = vim.fn.getcwd(),
+        hide = vim.fn.has("win32") == 1,
+        stdio = {stdin, stdout, stderr}
+    }, function (code, signal)
+            if code ~= 0 and code ~= 1 then
+                vim.notify("Code " .. code .. "; Signal " .. signal, vim.diagnostic.severity.WARN)
+            end
+    end)
+
+    if not handle then
+        stdin:close()
+        stdout:close()
+        stderr:close()
+        return
+    end
+
+    uv.read_start(stdout, function (err, data)
+        if err then
+            -- TODO
+        elseif data then
+            output = output .. data
+        else
+            local result = M._convert_checkstyle_output_to_diagnostic(namespace, output)
+            diagnostic_result = result.diagnostics
+            line_count = result.line_count
+            vim.schedule(function ()
+                vim.diagnostic.set(
+                    namespace,
+                    0,
+                    diagnostic_result,
+                    {
+                        virtual_text = true,
+                        severity_sort = true,
+                        update_in_insert = false
+                    }
+                )
+
+                local old_cmd_height = vim.o.cmdheight;
+                vim.o.cmdheight = old_cmd_height + 1;
+                if line_count == 0 then
+                    print("You are good to go; No violations from checkstyle found", "")
+                elseif line_count == 1 then
+                    print("Found", line_count, "check style violation")
+                else
+                    print("Found", line_count, "check style violations")
+                end
+                if old_cmd_height == nil then
+                    vim.o.cmdheight = 1;
+                else
+                    vim.o.cmdheight = old_cmd_height;
+                end
+            end)
+        end
+    end)
+end
+
+function M._convert_checkstyle_output_to_diagnostic(namespace, output)
+
     local diagnostics = {};
     local severity, line_n, line_col, message, d, line_count;
     line_count = 0
+
     for line in output:gmatch("[^\r\n]+") do
         if line:sub(0, 1) ~= '[' then
             goto continue
@@ -52,23 +113,12 @@ function M.java_checkstyle()
         _, _, line_col = string.find(line, ":%d+:(%d+):")
         _, _, message = string.find(line, ":%d+: ([^ ].*)")
 
-        if M.alwaysUseDefaultSeverity then
-            severity = M.defaultSeverity or "ERROR";
-        end
-
-        -- convert severity to vim diagnostic
-        local diagnosticSeverity = vim.diagnostic.severity.ERROR;
-        if severity == "INFO" then
-            diagnosticSeverity = vim.diagnostic.severity.INFO
-        elseif severity == "WARN" then
-            diagnosticSeverity = vim.diagnostic.severity.WARN;
-        end
 
         d = {
             bufnr = 0,
             lnum = tonumber(line_n) - 1,
             end_lnum = tonumber(line_n) - 1,
-            severity = diagnosticSeverity,
+            severity = vim.diagnostic.severity.ERROR,
             message = message,
             source = "checkstyle",
             namespace = namespace
@@ -82,32 +132,7 @@ function M.java_checkstyle()
         table.insert(diagnostics, d)
         ::continue::
     end
-
-    vim.diagnostic.set(
-        namespace,
-        0,
-        diagnostics,
-        {
-            virtual_text = true,
-            severity_sort = true,
-            update_in_insert = false
-        }
-    )
-
-    local old_cmd_height = vim.o.cmdheight;
-    vim.o.cmdheight = old_cmd_height + 1;
-    if line_count == 0 then
-        print("You are good to go; No violations from checkstyle found", "")
-    elseif line_count == 1 then
-        print("Found", line_count, "check style violation")
-    else
-        print("Found", line_count, "check style violations")
-    end
-    if old_cmd_height == nil then
-        vim.o.cmdheight = 1;
-    else
-        vim.o.cmdheight = old_cmd_height;
-    end
+    return {diagnostics=diagnostics, line_count=line_count}
 end
 
 return M;
